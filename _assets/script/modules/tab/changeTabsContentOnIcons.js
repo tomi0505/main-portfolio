e4b65e193d87b8ab688a68a1096dcc45796c626a
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-07-07 14:57:40 +0200

    I have fixed matchMedia.

--- _assets/script/modules/tab/changeTabsContentOnIcons.js
+++ _assets/script/modules/tab/changeTabsContentOnIcons.js
@@ -2,6 +2,8 @@ const originalValues = [];
 const newValuseWithIcon = ['home1', 'user1', 'tools', 'suitcase1', 'contact5'];
 const originTemplate = '<i class="icon-{{icon-name}} icon"></i>';
 
+const rwd = window.matchMedia("(max-width: 768px)");
+
 const saveOriginalValues = function(domElements) {
 	domElements.menu.forEach((li, i)=> {
 		originalValues.push(li.textContent);
@@ -9,16 +11,16 @@ const saveOriginalValues = function(domElements) {
 };
 
 const changeTabsContentOnIcons = function(domElements) {
-	if(window.innerWidth <= 768) {
-		domElements.menu.forEach((li, i)=> {
-			const newString = originTemplate.replace('{{icon-name}}', newValuseWithIcon[i]);
-			li.innerHTML = newString;
-		});
-	} else {
-		domElements.menu.forEach((li, i)=> {
-			li.innerHTML = originalValues[i];
-		});
-	}
+  if (rwd.matches) {
+    domElements.menu.forEach((li, i)=> {
+      const newString = originTemplate.replace('{{icon-name}}', newValuseWithIcon[i]);
+      li.innerHTML = newString;
+    });
+  } else {
+    domElements.menu.forEach((li, i)=> {
+      li.innerHTML = originalValues[i];
+    });
+  }
 };
 
 export default changeTabsContentOnIcons;
